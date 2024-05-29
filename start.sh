092cdc52d27dd357b838c29fb72c885ffa8012b1
Martijn Heins <114903145+6026961mborijnland@users.noreply.github.com>
2024-05-29 11:20:24 +0200

    lunix install fixed

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+python3 -m venv venv
+if [ ! -d "venv" ]; then
+    echo "Virtual environment does not exist. Please create it first."
+    exit 1
+fi
 
 echo "Activating virtual environment..."
 source venv/bin/activate
@@ -8,4 +12,5 @@ pip install -r requirements.txt
 
 echo "Running the application..."
 python run.py &
-xdg-open http://127.0.0.1:5000 || open http://127.0.0.1:5000
+
+xdg-open http://127.0.0.1:5000 |
